8be5900964f9c105764fe370419b6a1cd319e95f
Gaban <gabin.deboulogne@gmail.com>
2024-07-03 09:30:38 +0200

    Initial commit

---
+++ src/main/java/com/example/ecf4/Ecf4Application.java
@@ -0,0 +1,15 @@
+package com.example.ecf4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ecf4Application
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(Ecf4Application.class, args);
+	}
+
+}
